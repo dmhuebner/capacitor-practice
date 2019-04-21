08458c5b3f37e5c688562a0a440bd1e9bbf08fa0
David <davidmh.mail@gmail.com>
2019-04-20 23:38:46 -0500

    Implement get geolocation with capacitor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { bindCallback, Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
+
+const { Geolocation, Modals } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'capacitor-practice';
+export class AppComponent implements OnInit {
+
+  coords: Coordinates;
+
+  ngOnInit(): void {
+    this.watchPosition().subscribe(pos => {
+      this.coords = pos.coords;
+    });
+  }
+
+  watchPosition(): Observable<any> {
+    return bindCallback(Geolocation.watchPosition)({});
+  }
+
+  showAlert() {
+    const lat = this.coords.latitude;
+    const lng = this.coords.longitude;
+
+    Modals.alert({
+      title: 'Your Position',
+      message: `Lat: ${lat}, Lng: ${lng}`
+    });
+  }
 }
